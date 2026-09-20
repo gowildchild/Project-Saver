c28abcc5389f5e4f7bf98692db3e9d08107a63d4
Freaking Wildchild <32355099+gowildchild@users.noreply.github.com>
2026-09-21 01:17:26 +0200

    Update project_saver.py

--- master/project_saver.py
+++ master/project_saver.py
@@ -15,7 +15,7 @@ from bs4 import BeautifulSoup
 
 from project_saver_archive import process_html_content
 
-VERSION = "v0.0.73"
+VERSION = "v0.0.74"
 PORT = 19763
 EXPECTED_TOKEN = ""
 REPO_OWNER = "gowildchild"
@@ -69,7 +69,7 @@ def resolve_or_create_security_token(config_path="project_saver.cfg"):
             {
                 "url": "^https?://.*",
                 "profile": "Project Saver",
-                "autoSaveProfile": "__Disabled_Settings__"
+                "autoSaveProfile": "Project Saver"
             }
         ]
     }
