d2345e553c0b6b0e46798442753066ab5b24f807
Freaking Wildchild <32355099+gowildchild@users.noreply.github.com>
2026-09-20 15:37:10 +0200

    Update install.ps1

--- install.ps1
+++ install.ps1
@@ -1,5 +1,5 @@
 # ====================================================================================
-# PROJECT SAVER AUTOMATED NETWORK INSTALLATION & VALIDATION ENGINE
+# PROJECT SAVER AUTOMATED NETWORK INSTALLATION & VALIDATION ENGINE v0.4
 # irm https://raw.githubusercontent.com/gowildchild/Project-Saver/master/install.ps1 | iex
 # ====================================================================================
 $ErrorActionPreference = "Stop"
