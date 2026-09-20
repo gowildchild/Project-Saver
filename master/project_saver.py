50da95f70a6d47d363918e1bdc22ca5ffb8b833f
Freaking Wildchild <32355099+gowildchild@users.noreply.github.com>
2026-09-20 18:19:38 +0200

    Update project_saver.py

--- master/project_saver.py
+++ master/project_saver.py
@@ -14,7 +14,7 @@ from bs4 import BeautifulSoup
 
 from project_saver_archive import process_html_content
 
-VERSION = "v0.0.53-beta"
+VERSION = "v0.0.54-beta"
 PORT = 19763
 EXPECTED_TOKEN = ""
 REPO_OWNER = "gowildchild"
