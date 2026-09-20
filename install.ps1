9ac1588b5857e6b74af61307e28abcac744a01c0
Freaking Wildchild <32355099+gowildchild@users.noreply.github.com>
2026-09-20 21:01:35 +0200

    Update install.ps1

--- install.ps1
+++ install.ps1
@@ -2,7 +2,7 @@
 # PROJECT SAVER AUTOMATED NETWORK INSTALLATION & VALIDATION ENGINE
 # irm https://gowildchild.github.io/Project-Saver/install.ps1 | iex
 # ====================================================================================
-$InstallVersion = "v0.0.75"
+$InstallVersion = "v0.0.76"
 $ErrorActionPreference = "Stop"
 $RepoOwner = "gowildchild"
 $RepoName  = "Project-Saver"
