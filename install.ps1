6f2619ae77c2d9d544389e849f474e7a680fa392
Freaking Wildchild <32355099+gowildchild@users.noreply.github.com>
2026-09-20 22:31:04 +0200

    Update install.ps1

--- install.ps1
+++ install.ps1
@@ -2,7 +2,7 @@
 # PROJECT SAVER AUTOMATED NETWORK INSTALLATION & VALIDATION ENGINE
 # irm https://gowildchild.github.io/Project-Saver/install.ps1 | iex
 # ====================================================================================
-$InstallVersion = "v0.0.80-alpha"
+$InstallVersion = "v0.0.80-beta"
 $ErrorActionPreference = "Stop"
 $RepoOwner = "gowildchild"
 $RepoName  = "Project-Saver"
@@ -112,6 +112,14 @@ $SingleFileConfig = @{
             "saveToRestFormApiUrlFieldName"  = "url"
         }
     }
+    # THE RULES MATRIX: Forces the extension to treat 'Project Saver' as the default choice
+    "rules" = @(
+        @{
+            "url"             = "^https?://.*" # Match every HTTP and HTTPS web address layout natively
+            "profile"         = "Project Saver"
+            "autoSaveProfile" = "__Disabled_Settings__"
+        }
+    )
 } | ConvertTo-Json -Depth 4
 
 $JsonPath = Join-Path $InstallDir "singlefile-project-saver-config.json"
